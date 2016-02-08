fa6ff4d56f9b1e871958da0c2a2a25a73263b57b
Matthew Parker <matthew.j.parker96@gmail.com>
2016-02-08 05:48:21 +0000

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '8bac920438503f309a1d3ce145234ed329fdb03341e595edb9167e0ec5044b135a7b8084bf6e0bec9b84f8ef06151605368cdb15c15135cd2b1f68e72eff246f'
---
+++ db/seeds.rb
@@ -0,0 +1,40 @@
+Product.delete_all
+Product.create!(title: 'Duncan',
+description:
+%{<p>
+    Yogscast Duncan.
+  </p>},
+image_url: 'duncan.png',
+price: 49.95)
+
+Product.create!(title: 'lewis',
+description:
+%{<p>
+  Yogscast Lewis
+  </p>},
+image_url: 'lewis.png',
+price: 49.95)
+
+Product.create!(title: 'simon',
+description:
+%{<p>
+    Yogscast simon
+  </p>},
+image_url: 'simon.jpg',
+price: 49.95)
+
+Product.create!(title: 'Sips',
+description:
+%{<p>
+    Yogscast Sips
+  </p>},
+image_url: 'sips.png',
+price: 49.95)
+
+Product.create!(title: 'sjin_',
+description:
+%{<p>
+    Yogscast Sjin_
+  </p>},
+image_url: 'sjin.jpg',
+price: 49.95)
